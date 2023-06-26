82f839890842416ea97c072f3f72dcf56c30c435
tncks93 <tncks93@gmail.com>
2023-06-26 08:26:35 +0000

    chat-paging

--- chat/get_messages.php
+++ chat/get_messages.php
@@ -4,21 +4,83 @@
         require_once $_SERVER['DOCUMENT_ROOT']."/db/db.php";
 
         $room_id = $_GET['room_id'];
+        
         $user_id = $_SESSION['id'];
 
-        $query_select_messages = "SELECT c.*,(SELECT IF (c.to_id = $user_id,u.user_image,NULL) FROM user u WHERE u.user_id = c.from_id) AS op_image 
-        FROM chat c WHERE c.room_id = $room_id ORDER BY c.chat_id;";
+        $messages = array();
+        $res = array();
 
-        if($result = mysqli_query($db_connect,$query_select_messages)){
-            $messages = array();
-            while($message = mysqli_fetch_assoc($result)){
-                $messages[] = $message;
+        if($_GET['book_mark'] == null){
+
+            //bookmark null 일 때
+            $query_select_messages = "SELECT c.*,(SELECT IF (c.to_id = $user_id,u.user_image,NULL) FROM user u WHERE u.user_id = c.from_id) AS op_image
+            FROM chat c WHERE c.room_id = $room_id ORDER BY c.chat_id DESC LIMIT 20";
+
+            if($result = mysqli_query($db_connect,$query_select_messages)){
+                while($message = mysqli_fetch_assoc($result)){
+                    $messages[] = $message;
             }
 
-            echo json_encode($messages,JSON_UNESCAPED_UNICODE);
+            $messages = array_reverse($messages,false);
+            $is_last = true;
+
+            $res['messages'] = $messages;
+            $res['is_last'] = $is_last;
+            echo json_encode($res,JSON_UNESCAPED_UNICODE);
+
+
+            }else{
+                echo json_encode($db_connect,JSON_UNESCAPED_UNICODE);
+            }
+
+
+
         }else{
-            echo json_encode($mysqli_error($db_connect),JSON_UNESCAPED_UNICODE);
+            $book_mark = $_GET['book_mark'];
+
+            $query_select_messages_front = "SELECT c.*,(SELECT IF (c.to_id = $user_id,u.user_image,NULL) FROM user u WHERE u.user_id = c.from_id) AS op_image
+            FROM chat c WHERE (c.room_id = $room_id AND c.sent_at < '$book_mark') ORDER BY c.chat_id DESC LIMIT 10";
+
+            $query_select_messages_back = "SELECT c.*,(SELECT IF (c.to_id = $user_id,u.user_image,NULL) FROM user u WHERE u.user_id = c.from_id) AS op_image
+            FROM chat c WHERE (c.room_id = $room_id AND c.sent_at >= '$book_mark') ORDER BY c.chat_id LIMIT 10";
+
+            $query_select_last_messages_id = "SELECT MAX(chat_id) AS last_id FROM chat WHERE room_id = $room_id";
+
+            $result_front = mysqli_query($db_connect,$query_select_messages_front);
+            $result_back = mysqli_query($db_connect,$query_select_messages_back);
+
+            $messages_front = array();
+            $messages_back = array();
+
+            while($msg_front = mysqli_fetch_assoc($result_front)){
+                $messages_front[] = $msg_front;
+            }
+
+            $messages_front = array_reverse($messages_front,false);
+
+            while($msg_back = mysqli_fetch_assoc($result_back)){
+                $messages_back[] = $msg_back;
+            }
+
+            $messages = array_merge($messages_front,$messages_back);
+
+            $last_id = mysqli_fetch_assoc(mysqli_query($db_connect,$query_select_last_messages_id))['last_id'];
+            $is_last;
+
+            if($last_id == end($messages)['chat_id']){
+                $is_last = true;
+            }else{
+                $is_last = false;
+            }
+
+            $res['messages'] = $messages;
+            $res['is_last'] = $is_last;
+            echo json_encode($res,JSON_UNESCAPED_UNICODE);
         }
+
+        
+
+        
 
 
     }else{
---
+++ chat/get_messages_paging.php
@@ -0,0 +1,79 @@
+<?php
+    if(isset($_GET['room_id'])){
+        require_once $_SERVER['DOCUMENT_ROOT']."/login/session.php";
+        require_once $_SERVER['DOCUMENT_ROOT']."/db/db.php";
+
+        define('MODE_FRONT','front');
+        define('MODE_BACK','back');
+
+        $room_id = $_GET['room_id'];
+        $paging_idx = $_GET['page_idx'];
+        $paging_mode = $_GET['paging_mode'];
+        $user_id = $_SESSION['id'];
+
+        $messages = array();
+        $res = array();
+        
+        $query_select_paging = "SELECT c.*,(SELECT IF (c.to_id = $user_id,u.user_image,NULL) FROM user u WHERE u.user_id = c.from_id) AS op_image
+        FROM chat c WHERE (c.room_id = $room_id AND %s) ORDER BY c.chat_id%s LIMIT 10";
+
+        switch($paging_mode){
+            case MODE_FRONT : 
+                $where_paging = "c.sent_at < '$paging_idx'";
+                $query_select_paging = sprintf($query_select_paging,$where_paging," DESC");
+
+                $result = mysqli_query($db_connect,$query_select_paging);
+                while($message = mysqli_fetch_assoc($result)){
+                    $messages[] = $message;
+                }
+
+                $messages = array_reverse($messages,false);
+
+                $query_select_first_messages_id = "SELECT MIN(chat_id) AS first_id FROM chat WHERE room_id = $room_id";
+
+                $first_id = mysqli_fetch_assoc(mysqli_query($db_connect,$query_select_first_messages_id))['first_id'];
+
+                if($messages[0]['chat_id'] == $first_id){
+                    $res['is_first'] = true;
+                }else{
+                    $res['is_first'] = false;
+                }
+
+                break;
+
+            case MODE_BACK : 
+                $where_paging = "c.sent_at > '$paging_idx'";
+                $query_select_paging = sprintf($query_select_paging,$where_paging,"");
+
+                $query_select_last_messages_id = "SELECT MAX(chat_id) AS last_id FROM chat WHERE room_id = $room_id";
+
+                $result = mysqli_query($db_connect,$query_select_paging);
+                while($message = mysqli_fetch_assoc($result)){
+                    $messages[] = $message;
+                }
+
+                $last_id = mysqli_fetch_assoc(mysqli_query($db_connect,$query_select_last_messages_id))['last_id'];
+
+                if(end($messages)['chat_id'] == $last_id){
+                    $res['is_last'] = true;
+                }else{
+                    $res['is_last'] = false;
+                }
+
+                break;
+
+            default :
+                exit();
+                break;
+        }
+
+        $res['messages'] = $messages;
+
+        echo json_encode($res,JSON_UNESCAPED_UNICODE);
+    }else{
+        exit();
+    }
+
+
+
+?>
